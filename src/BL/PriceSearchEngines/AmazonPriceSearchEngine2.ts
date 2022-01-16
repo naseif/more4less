799779e8a84ecd3bd6020b40368086dd05d348cb
stho32 <stho32github@gmail.com>
2022-01-16 19:18:44 +0100

    added more stuff for experimentation

--- src/BL/PriceSearchEngines/AmazonPriceSearchEngine2.ts
+++ src/BL/PriceSearchEngines/AmazonPriceSearchEngine2.ts
@@ -1,13 +1,26 @@
-import { SearchResult } from "..";
-import { ISearchResult } from "../../Interfaces/index";
 import { SearchEngineBase } from "./SearchEngineBase";
+const cheerio = require("cheerio");
 
 export class AmazonPriceSearchEngine2 extends SearchEngineBase {
     async search(searchTerm: string): Promise<any> {
         const baseUrl = "https://www.amazon.de";
         const $ = await this.requestWebsite(`${baseUrl}/s?k=`, searchTerm);
 
-        let test = $("div")
-        console.log(test)
+        //writeFileSync("test.html", $, "utf8");
+
+        let divs = $(".s-result-item")
+        for (let i = 0; i < divs.length; i++) {
+            const thisDiv = divs[i];
+            const htmlOfThisDiv = $.html(thisDiv);
+            const $detail = cheerio.load(htmlOfThisDiv, {
+                xmlMode: true,
+            });
+
+            console.log("---------------------------------------------------------");
+            console.log(i);
+            console.log($detail(".s-title-instructions-style").text().trim());
+            console.log($detail(".a-price-whole").text().trim());
+            
+        }
     }
 }
