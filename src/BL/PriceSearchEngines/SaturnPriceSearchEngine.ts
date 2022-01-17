9d1340b5740450de0bfb588ec234d424083a26ee
naseif <d2mds21@gmail.com>
2022-01-17 09:32:35 +0000

    formatted files

--- src/BL/PriceSearchEngines/SaturnPriceSearchEngine.ts
+++ src/BL/PriceSearchEngines/SaturnPriceSearchEngine.ts
@@ -7,7 +7,7 @@ export class SaturnPriceSearchEngine extends SearchEngineBase {
     async search(searchTerm: string): Promise<ISearchResult[]> {
         const baseUrl = 'https://www.saturn.de';
         const $ = await this.requestWebsite(baseUrl + '/de/search.html?query=', searchTerm);
-        const resultDivs = $("div[data-test=mms-search-srp-productlist-item]")
+        const resultDivs = $('div[data-test=mms-search-srp-productlist-item]');
 
         let titles: any[] = [];
         let links: any[] = [];
@@ -20,10 +20,10 @@ export class SaturnPriceSearchEngine extends SearchEngineBase {
             const $detail = cheerio.load(htmlOfThisDiv, {
                 xmlMode: true
             });
-            titles.push($detail("h2[data-test=product-title]").text());
-            ratings.push($detail("div[data-test=mms-customer-rating]").text());
-            prices.push($detail("div[data-test=mms-unbranded-price]").text());
-            links.push(baseUrl + $detail("a[data-test=mms-router-link]").attr('href'));
+            titles.push($detail('h2[data-test=product-title]').text());
+            ratings.push($detail('div[data-test=mms-customer-rating]').text());
+            prices.push($detail('div[data-test=mms-unbranded-price]').text());
+            links.push(baseUrl + $detail('a[data-test=mms-router-link]').attr('href'));
         }
 
         let result: ISearchResult[] = [];
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import * as bl from './BL/index';
-import * as interfaces from "./Interfaces/index"
-const searchEngines = new bl.SearchEngineList([new bl.MediaMarktPriceSearchEngine(), new bl.AmazonPriceSearchEngine2(), new bl.EbayPriceSearchEngine(), new bl.SaturnPriceSearchEngine()]);
+import * as interfaces from './Interfaces/index';
+const searchEngines = new bl.SearchEngineList([
+    new bl.MediaMarktPriceSearchEngine(),
+    new bl.AmazonPriceSearchEngine2(),
+    new bl.EbayPriceSearchEngine(),
+    new bl.SaturnPriceSearchEngine()
+]);
 
 (async () => {
     var searchResults = await searchEngines.search('firestick');
-    const output = new bl.JSONOutput("test.json")
-    output.outputData(searchResults)
+    const output = new bl.JSONOutput('test.json');
+    output.outputData(searchResults);
     console.log(searchResults);
 })();
 
-
 //const output : interfaces.IOutput = new bl.ConsoleOutput();
 // const output: interfaces.IOutput = new bl.CsvOutput("test.csv");
-
