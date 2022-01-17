3eaf865beb8cad00317e1f981f0222d955fe2927
naseif <d2mds21@gmail.com>
2022-01-17 11:07:04 +0000

    Added docs, uploaded the scraper to npm

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,2 @@
-import * as bl from './BL/index';
-import * as interfaces from './Interfaces/index';
-const searchEngines = new bl.SearchEngineList([
-    new bl.MediaMarktPriceSearchEngine(),
-    new bl.AmazonPriceSearchEngine2(),
-    new bl.EbayPriceSearchEngine(),
-    new bl.SaturnPriceSearchEngine()
-]);
-
-(async () => {
-    var searchResults = await searchEngines.search('firestick');
-    const output = new bl.JSONOutput('test.json');
-    console.log(searchResults);
-})();
-
-//const output : interfaces.IOutput = new bl.ConsoleOutput();
-// const output: interfaces.IOutput = new bl.CsvOutput("test.csv");
+export * from "./BL"
+export * from "./Interfaces"
