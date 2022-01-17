7d0f297d71425b12ba474fb7d8c223c34cc1860a
naseif <d2mds21@gmail.com>
2022-01-17 09:31:28 +0000

    implemented `MediaMarktPriceSearchEngine` and `SaturnPriceSearchEngine`. Saturn and MediaMarkt engines are 100% identical

--- src/BL/PriceSearchEngines/MediaMarktPriceSearchEngine.ts
+++ src/BL/PriceSearchEngines/MediaMarktPriceSearchEngine.ts
@@ -1,8 +1,37 @@
+import { SearchResult } from '..';
 import { ISearchResult } from '../../Interfaces/index';
 import { SearchEngineBase } from './SearchEngineBase';
+const cheerio = require('cheerio');
 
-class MediaMarktPriceSearchEngine extends SearchEngineBase {
-    search(searchTerm: string): Promise<ISearchResult[]> {
-        throw new Error('Method not implemented.');
+export class MediaMarktPriceSearchEngine extends SearchEngineBase {
+    async search(searchTerm: string): Promise<ISearchResult[]> {
+        const baseUrl = 'https://www.mediamarkt.de';
+        const $ = await this.requestWebsite(baseUrl + '/de/search.html?query=', searchTerm);
+        const resultDivs = $("div[data-test=mms-search-srp-productlist-item]")
+
+        let titles: any[] = [];
+        let links: any[] = [];
+        let prices: any[] = [];
+        let ratings: any[] = [];
+
+        for (let i = 0; i < resultDivs.length; i++) {
+            const thisResult = resultDivs[i];
+            const htmlOfThisDiv = $.html(thisResult);
+            const $detail = cheerio.load(htmlOfThisDiv, {
+                xmlMode: true
+            });
+            titles.push($detail("h2[data-test=product-title]").text());
+            ratings.push($detail("div[data-test=mms-customer-rating]").text());
+            prices.push($detail("div[data-test=mms-unbranded-price]").text());
+            links.push(baseUrl + $detail("a[data-test=mms-router-link]").attr('href'));
+        }
+
+        let result: ISearchResult[] = [];
+
+        titles.forEach((title: string, index: number) => {
+            result.push(new SearchResult(this.constructor.name, links[index], title, ratings[index], prices[index]));
+        });
+
+        return result;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import * as bl from './BL/index';
 import * as interfaces from "./Interfaces/index"
-const searchEngines = new bl.SearchEngineList([new bl.EbayPriceSearchEngine(), new bl.AmazonPriceSearchEngine2()]);
+const searchEngines = new bl.SearchEngineList([new bl.MediaMarktPriceSearchEngine(), new bl.AmazonPriceSearchEngine2(), new bl.EbayPriceSearchEngine(), new bl.SaturnPriceSearchEngine()]);
 
 (async () => {
     var searchResults = await searchEngines.search('firestick');
-    const output: interfaces.IOutput = new bl.JSONOutput("test.json");
-
-    output.outputData(searchResults);
+    const output = new bl.JSONOutput("test.json")
+    output.outputData(searchResults)
     console.log(searchResults);
 })();
 
 
-// //const output : interfaces.IOutput = new bl.ConsoleOutput();
-// // const output: interfaces.IOutput = new bl.CsvOutput("test.csv");
+//const output : interfaces.IOutput = new bl.ConsoleOutput();
+// const output: interfaces.IOutput = new bl.CsvOutput("test.csv");
 
