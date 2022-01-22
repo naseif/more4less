6ed92675c4ed3c787f0354c908e2f629c6e30039
stho32 <stho32github@gmail.com>
2022-01-22 15:14:54 +0100

    change the list to call all search engines in parallel

--- src/BL/PriceSearchEngines/SearchEngineList.ts
+++ src/BL/PriceSearchEngines/SearchEngineList.ts
@@ -24,13 +24,14 @@ export class SearchEngineList implements ISearchEngine {
     async search(searchTerm: string): Promise<ISearchResult[]> {
         let result: ISearchResult[] = [];
 
+        let searchPromises = [];
+                
         for (const engine of this.searchEngines) {
-            const temp = await engine.search(searchTerm);
-
-            for (const searchResult of temp) {
-                result.push(searchResult);
-            }
+            searchPromises.push(engine.search(searchTerm));
         }
+
+        const getAll = await Promise.all(searchPromises);
+        result = getAll.flat(1);
 
         return result;
     }
